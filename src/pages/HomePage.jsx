895b3353657820f126430a82e9c4a43b4a4f79e0
Gaz Cocklin <gazc@cy-sec.co.uk>
2026-04-10 17:57:19 +0000

    feat: remove ServicesGrid, move Platforms directly below hero

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import LandingPageHero from '@/components/LandingPageHero';
-import ServicesGrid from '@/components/ServicesGrid';
-import WhyCySecSection from '@/components/WhyCySecSection';
 import PlatformsShowcase from '@/components/PlatformsShowcase';
+import WhyCySecSection from '@/components/WhyCySecSection';
 import BottomCTASection from '@/components/BottomCTASection';
 
 const HomePage = () => (
@@ -13,9 +12,8 @@ const HomePage = () => (
       <meta name="description" content="vCISO leadership, DORA & NIS2 compliance sprints, CompTIA certified training, and the FortifyOne compliance platform. One trusted partner." />
     </Helmet>
     <LandingPageHero />
-    <ServicesGrid />
-    <WhyCySecSection />
     <PlatformsShowcase />
+    <WhyCySecSection />
     <BottomCTASection />
   </div>
 );
